9f886c7ef915f1381e51073cd2c60055ebe28429
naeohmi <nameyer@adobe.com>
2020-11-19 09:40:37 -0800

    add mkdir new api and change all article- to article_ and clean up

--- app.js
+++ app.js
@@ -2,7 +2,17 @@
 
 const fs = require('fs');
 
-fs.readFile('./languages/en_us.json', (err, data) => {
+const currentLocale = 'zh_cn';
+const fileToRead = `./translations/${currentLocale}.json`;
+
+let newFilePath = `./api/${currentLocale}`;
+console.log('test')
+fs.mkdir(newFilePath, { recursive: true }, (err) => {
+    if (err) throw err;
+});
+
+
+fs.readFile(fileToRead, (err, data) => {
     if (err) throw err;
     let jsonData = JSON.parse(data);
     console.log(jsonData);
@@ -10,7 +20,7 @@ fs.readFile('./languages/en_us.json', (err, data) => {
     for (let key in jsonData) {
         if (jsonData[key]) {
             console.log(`Writing ${key} new file`)
-            const fileName = `${key}.json`;
+            const fileName = `${newFilePath}/${key}.json`;
             fs.writeFileSync(fileName, jsonData[key]);
         } else {
             console.log(` Error - not able to write ${key} new file`)
